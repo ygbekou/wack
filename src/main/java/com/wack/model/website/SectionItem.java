6f1586985c0d26a56c4fd838b9853a9794a6a34b
Panawe Batanado <panawe.batanado@ipnetinstitute.com>
2020-06-06 03:37:30 -0400

    Change

--- src/main/java/com/wack/model/website/SectionItem.java
+++ src/main/java/com/wack/model/website/SectionItem.java
@@ -13,34 +13,42 @@ import javax.persistence.Transient;
 import com.wack.model.BaseEntity;
 import com.wack.util.Utils;
 
-
 @Entity
-@Table(name="SECTION_ITEM")
+@Table(name = "SECTION_ITEM")
 public class SectionItem extends BaseEntity {
-	
+
 	@Id
-	@Column(name ="SECTION_ITEM_ID")
+	@Column(name = "SECTION_ITEM_ID")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@ManyToOne
 	@JoinColumn(name = "SECTION_ID")
 	private Section section;
-	
+
 	private String title;
-	
+
+	private String name;
+
 	private String description;
-	
-	@Column(name ="PICTURE")
+
+	@Column(name = "PICTURE")
 	private String picture;
-	
+
 	private String summary;
 
 	private Integer status;
-	
+
 	private String language;
 
-	
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
 	public String getSummary() {
 		return summary;
 	}
@@ -81,7 +89,6 @@ public class SectionItem extends BaseEntity {
 		this.description = description;
 	}
 
-
 	public String getPicture() {
 		return picture;
 	}
@@ -105,42 +112,43 @@ public class SectionItem extends BaseEntity {
 	public void setLanguage(String language) {
 		this.language = language;
 	}
-	
+
 	public String getSectionName() {
 		return this.getSection().getName();
 	}
-	
+
 	public String getStatusDesc() {
 		return status == 0 ? "Actif" : "Inactif";
 	}
-	
-	
+
 	// Transient
 	public String getText1() {
-		String[] texts = description.split("\\|");
-		
-		return texts.length > 0 ? texts[0] : "" ;
+		String[] texts = description == null ? null : description.split("\\|");
+
+		return texts != null && texts.length > 0 ? texts[0] : "";
 	}
-	
+
 	public String getText2() {
-		String[] texts = description.split("\\|");
-		
-		return texts.length > 1 ? texts[1] : "" ;
+		String[] texts = description == null ? null : description.split("\\|");
+
+		return texts != null && texts.length > 1 ? texts[1] : "";
 	}
-	
+
 	public String getText3() {
-		String[] texts = description.split("\\|");
-		
-		return texts.length > 2 ? texts[2] : "" ;
+		String[] texts = description == null ? null : description.split("\\|");
+
+		return texts != null && texts.length > 2 ? texts[2] : "";
 	}
 
 	@Transient
 	public String getShortMessage() {
-		return description != null && description.length() > 100 ? Utils.truncateHTML(description,100,null) : description;
+		return description != null && description.length() > 100 ? Utils.truncateHTML(description, 100, null)
+				: description;
 	}
 
 	@Transient
 	public String getMediumMessage() {
-		return description != null && description.length() > 200 ? Utils.truncateHTML(description,200,null) : description;
+		return description != null && description.length() > 200 ? Utils.truncateHTML(description, 200, null)
+				: description;
 	}
 }
